28d59c5db7394cc368ead2cc769a81319bddd3cb
Aldo Grabić <aldo.grabic99@gmail.com>
2023-06-27 21:30:41 +0200

    logika za sidebar gumbe

--- target/classes/static/filtering.js
+++ target/classes/static/filtering.js
@@ -1,73 +1,95 @@
 var priceFrom = document.getElementById("priceFrom");
 var priceTo = document.getElementById("priceTo");
 
-
-
-$(document).ready(function() {
-    $('#vehicleType').change(function() {
-      loadManufacturers();
-    });
-    
-    $('#manufacturer').change(function() {
-      loadModels();
-    });
+document.getElementById("transmissionButtons").addEventListener("click", function(event) {
+  var buttonId = event.target.id;
+  selectTransmissionButton(buttonId);
 });
 
+document.getElementById("userTypeButtons").addEventListener("click", function(event) {
+  var buttonId = event.target.id;
+  selectUserTypeButton(buttonId);
+});
+
+$(document).ready(function() {
+  $('#vehicleType').change(function() {
+    loadManufacturers();
+  });
+
+  $('#manufacturer').change(function() {
+    loadModels();
+  });
+});
 
 priceFrom.addEventListener("input", validateInput);
 priceTo.addEventListener("input", validateInput);
 
 function validateInput(event) {
-    var input = event.target;
-    var value = input.value;
-    var sanitizedValue = value.replace(/[^0-9]/g, '');
-    input.value = sanitizedValue;
-    
+  var input = event.target;
+  var value = input.value;
+  var sanitizedValue = value.replace(/[^0-9]/g, "");
+  input.value = sanitizedValue;
 }
-//dodat jos da nemre bit tocka, zarez i te pizdarije
-  
-
+// Additional validation to disallow dots, commas, and other characters
 
 function loadManufacturers() {
-    var selectedVehicleType = $('#vehicleType').val();
-    if (selectedVehicleType === "") {
+  var selectedVehicleType = $('#vehicleType').val();
+  if (selectedVehicleType === "") {
+    $('#manufacturer').empty();
+    $('#manufacturer').append('<option value="" disabled selected>Odabir</option>');
+  } else {
+    $.ajax({
+      url: '/manufacturers',
+      type: 'POST',
+      data: selectedVehicleType,
+      contentType: 'text/plain',
+      success: function(response) {
         $('#manufacturer').empty();
         $('#manufacturer').append('<option value="" disabled selected>Odabir</option>');
-    } else {
-        $.ajax({
-            url: '/manufacturers',
-            type: 'POST',
-            data: selectedVehicleType,
-            contentType: 'text/plain',
-            success: function(response) {
-                $('#manufacturer').empty();
-                $('#manufacturer').append('<option value="" disabled selected>Odabir</option>');
-                response.forEach(function(manufacturer) {
-                    $('#manufacturer').append('<option value="' + manufacturer + '">' + manufacturer + '</option>');
-                });
-            },
-            error: function(xhr, status, error) {
-                // Handle error, if any
-            }
+        response.forEach(function(manufacturer) {
+          $('#manufacturer').append('<option value="' + manufacturer + '">' + manufacturer + '</option>');
         });
-    }
+      },
+      error: function(xhr, status, error) {
+        // Handle error, if any
+      }
+    });
+  }
 }
 
 function loadModels() {
-    var selectedManufacturer = $('#manufacturer').val();
+  var selectedManufacturer = $('#manufacturer').val();
 
-    $.ajax({
-        url: '/models',
-        type: 'POST',
-        data: selectedManufacturer,
-        contentType: 'text/plain',
-        success: function(response) {
-            $('#model').html(response);
-        },
-        error: function(xhr, status, error) {
-            // Handle error, if any
-        }
-    });
+  $.ajax({
+    url: '/models',
+    type: 'POST',
+    data: selectedManufacturer,
+    contentType: 'text/plain',
+    success: function(response) {
+      $('#model').html(response);
+    },
+    error: function(xhr, status, error) {
+      // Handle error, if any
+    }
+  });
 }
 
-//funkcija da gumb bude kliknut
+function selectTransmissionButton(buttonId) {
+    var buttons = document.querySelectorAll('#transmissionButtons button');
+    buttons.forEach(function(button) {
+      button.classList.remove('selected');
+    });
+  
+    var selectedButton = document.getElementById(buttonId);
+    selectedButton.classList.add('selected');
+  }
+  
+  function selectUserTypeButton(buttonId) {
+    var buttons = document.querySelectorAll('#userTypeButtons button');
+    buttons.forEach(function(button) {
+      button.classList.remove('selected');
+    });
+  
+    var selectedButton = document.getElementById(buttonId);
+    selectedButton.classList.add('selected');
+}
