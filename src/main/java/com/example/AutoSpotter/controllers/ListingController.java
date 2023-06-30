554d529737718f398d1684c0bffbad72f3d96945
Aldo Grabić <aldo.grabic99@gmail.com>
2023-06-30 13:27:49 +0200

    dodan korisnicki racun i detalji oglasa

--- src/main/java/com/example/AutoSpotter/controllers/ListingController.java
+++ src/main/java/com/example/AutoSpotter/controllers/ListingController.java
@@ -1,7 +1,26 @@
 package com.example.AutoSpotter.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.AutoSpotter.classes.listing.Listing;
+import com.example.AutoSpotter.classes.listing.ListingRepository;
 
 @Controller
 public class ListingController {
+    
+    private ListingRepository listingRepository;
+
+    public ListingController(ListingRepository listingRepository) {
+        this.listingRepository = listingRepository;
+    }
+    
+    @GetMapping("/oglasi/{listingId}")
+    public String getListingDetails(@PathVariable("listingId") int listingId, Model model) {
+        Listing listing = listingRepository.getListingById(listingId);
+        model.addAttribute("listing", listing);
+        return "listing-details";
+    }
 }
--- src/main/java/com/example/AutoSpotter/controllers/UserProfileController.java
+++ src/main/java/com/example/AutoSpotter/controllers/UserProfileController.java
@@ -1,9 +1,26 @@
 package com.example.AutoSpotter.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.AutoSpotter.classes.user.User;
+import com.example.AutoSpotter.classes.user.UserRepository;
 
 @Controller
 public class UserProfileController {
 
-}
+    private UserRepository userRepository;
 
+    public UserProfileController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/korisnicki-profil/{userId}")
+    public String showUserProfile(@PathVariable("userId") int userId, Model model) {
+        User user = userRepository.getUserById(userId);
+        model.addAttribute("user", user);
+        return "user-profile";
+    }
+}
