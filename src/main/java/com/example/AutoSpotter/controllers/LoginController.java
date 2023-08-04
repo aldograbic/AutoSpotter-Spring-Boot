8250c30e35123d1968c981fe8c23883b475183fc
Aldo Grabić <aldo.grabic99@gmail.com>
2023-08-04 15:21:32 +0200

    select2

--- src/main/java/com/example/AutoSpotter/controllers/LoginController.java
+++ src/main/java/com/example/AutoSpotter/controllers/LoginController.java
@@ -9,7 +9,6 @@ import org.springframework.security.web.authentication.logout.SecurityContextLog
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
