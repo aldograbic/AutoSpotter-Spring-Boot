cf6cf0d28168399c8de9c8f3ab28453761bac757
Goran Roso <fanatic.roso@gmail.com>
2023-09-09 18:30:44 +0200

    Majmuncic dodan, al smo ga zakomentirali

---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,24 @@
+// var x = document.getElementById("hands");
+// var y = document.getElementById("animcon");
+
+// function closeEyes() {
+//     y.style.backgroundImage = "url(https://raw.githubusercontent.com/naaficodes/Monkey-Login/master/images/monkey_pwd.gif)"; // Adjust the path to your monkey_pwd.gif
+//     x.style.marginTop = "0%";
+// }
+
+// function openEyes() {
+//     y.style.backgroundImage = "url(https://raw.githubusercontent.com/naaficodes/Monkey-Login/master/images/monkey.gif)"; // Adjust the path to your monkey.gif
+//     x.style.marginTop = "110%";
+// }
+
+// document.getElementById("username").addEventListener("focus", function () {
+//     openEyes();
+// });
+
+// document.getElementById("password").addEventListener("focus", function () {
+//     closeEyes();
+// });
+
+// document.getElementById("password").addEventListener("blur", function () {
+//     openEyes();
+// });
