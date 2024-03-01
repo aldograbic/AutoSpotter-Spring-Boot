9ed25820485087edd97834d89b8c1a571f8aa791
Aldo Grabić <aldo.grabic99@gmail.com>
2024-03-01 21:05:36 +0100

    Micanje osjetljivih podataka s GitHuba

--- src/main/java/com/example/AutoSpotter/config/GoogleCloudStorageConfig.java
+++ src/main/java/com/example/AutoSpotter/config/GoogleCloudStorageConfig.java
@@ -4,16 +4,21 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.auth.oauth2.ServiceAccountCredentials;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import java.io.IOException;
 
 @Configuration
 public class GoogleCloudStorageConfig {
+
+    @Value("${google.cloud.creds}")
+    private String creds;
     
     @Bean
     public StorageOptions storageOptions() throws IOException {
-        GoogleCredentials credentials = ServiceAccountCredentials.fromStream(getClass().getResourceAsStream("/peerless-tiger-394915-c4668df1d81d.json"));
+        GoogleCredentials credentials = ServiceAccountCredentials.fromStream(getClass().getResourceAsStream(creds));
         return StorageOptions.newBuilder().setCredentials(credentials).build();
     }
 
--- src/main/java/com/example/AutoSpotter/config/MailConfig.java
+++ src/main/java/com/example/AutoSpotter/config/MailConfig.java
@@ -2,6 +2,7 @@ package com.example.AutoSpotter.config;
 
 import java.util.Properties;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,13 +11,19 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 @Configuration
 public class MailConfig {
 
+    @Value("${spring.mail.username}")
+    private String email;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
-        mailSender.setUsername("autospotter.contact@gmail.com");
-        mailSender.setPassword("aacihwwjcpazwdrk");
+        mailSender.setUsername(email);
+        mailSender.setPassword(password);
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
